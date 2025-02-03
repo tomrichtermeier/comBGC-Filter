bb11173b55f603910c8143dd0019a39f5cac6d2d
Tom Richtermeier <tom.richtermeier@gmx.de>
2025-02-03 12:33:32 +0100

    Updated .gitignore to exclude build artifacts and compiled files

--- combgc/build/lib/functions/__init__.py
+++
@@ -1,3 +0,0 @@
-from .filter import filter_bgc, cleanup_table, combine_tool, process_chunk, parallelization
-
-__all__ = ["filter_bgc", "cleanup_table", "combine_tool", "process_chunk", "parallelization"]
